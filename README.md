8db0b05ec82fbf780f592b3c882cd956c42f08f0
Luka Blašković <59925899+lukablaskovic@users.noreply.github.com>
2026-01-28 13:24:46 +0100

    updated template

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # wa-final-template
+
 Predložak za pisanje 2. kolokvija iz Web aplikacija
+
+## To-Do
+
+Pokrenite `npm install` i izradite `.env` datoteku u _root_ direktoriju projekta.
+
+- Podesite MongoDB atlas cluster prema uputama iz skripte WA5.
+- Izradite korisnika, zabilježite lozinku i podesite 0.0.0.0/0 IP whitelist.
+- Kopirajte connection string iz MongoDB atlas sučelja i zalijepite ga u `.env` datoteku pod `MONGODB_URI` varijablu.
+- Izradite novu bazu podataka naziva `wa-final` i tražene kolekcije iz zadatka.
+- Preporuka je koristiti aplikaciju MongoDB Compass ili VS Code Mongo ekstenziju za brži pregled baze podataka.
+
+Ako sve radi, spremni ste za pisanje kolokvija.
+
+## Pokretanje poslužitelja
+
+- `npm run start` - ekvivalentno `node index.js`
+- `npm run dev` - ekvivalentno `nodemon index.js` (automatsko ponovno pokretanje pri promjeni koda - preporuka)
--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-import express from 'express';
+import express from "express";
 
-import { connectToDatabase } from './db.js';
+import { connectToDatabase } from "./db.js";
 
 const app = express();
 
 app.use(express.json());
 
-const db = await connectToDatabase();
+const db = await connectToDatabase(); // primjer korištenja asinkrone funkcije za spajanje na bazu podataka
 
-app.get('/', (req, res) => {
-  res.send('wa-final');
+app.get("/", (req, res) => {
+  res.send("wa-final backend radi!");
 });
 
-const PORT = 3000;
+const PORT = 3000; // pripazite da je port slobodan, ako nije, promijenite ga
 
-app.listen(PORT, error => {
+app.listen(PORT, (error) => {
   if (error) {
-    console.log('Greška prilikom pokretanja poslužitelja', error);
+    console.log("Greška prilikom pokretanja poslužitelja", error);
   }
   console.log(`Poslužitelj sluša na http://localhost:${PORT}`);
 });
